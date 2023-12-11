d08c5fbc418d0259f8c895639e24d0a138366096
Aleksey02 <lyosha2002stich@gmail.com>
2023-12-11 20:30:55 +0500

    feat: Add js and color theme switch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-document.documentElement.style.setProperty('$yellow-main', '#2ecc71');
+let body = document.querySelector("body");
+let btnColorTheme = document.querySelector(".header__menu");
+
+btnColorTheme.addEventListener("click", function () {
+    if(body.classList.contains("gray")) {
+        body.classList.remove("gray");
+        body.classList.add("blue");
+    } else if(body.classList.contains("blue")) {
+        body.classList.remove("blue");
+        body.classList.add("red");
+    } else if(body.classList.contains("red")) {
+        body.classList.remove("red");
+        body.removeAttribute('class');
+    }else{
+        body.classList.add("gray");
+    }
+})
