cb7fd7205afc526563a4d226bcaf9c66be4b52bd
Aleksey02 <lyosha2002stich@gmail.com>
2023-12-12 21:36:46 +0500

    fix: fix js for switch theme color

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
+const themeColors = ['gray', 'blue', 'red'];
+
 let body = document.querySelector("body");
 let btnColorTheme = document.querySelector(".header__menu");
 
 btnColorTheme.addEventListener("click", function () {
-    if(body.classList.contains("gray")) {
-        body.classList.remove("gray");
-        body.classList.add("blue");
-    } else if(body.classList.contains("blue")) {
-        body.classList.remove("blue");
-        body.classList.add("red");
-    } else if(body.classList.contains("red")) {
-        body.classList.remove("red");
-        body.removeAttribute('class');
-    }else{
-        body.classList.add("gray");
+    let currentTheme = body.classList.value;
+    themeColors.forEach((color, index)=>{
+        if(color === currentTheme){
+            body.classList.remove(color);
+            if((index + 1) < themeColors.length){
+                body.classList.add(themeColors[index + 1]);
+            }else{
+                body.removeAttribute('class');
+            }
+        }
+    })
+    if(currentTheme===''){
+        body.classList.add(themeColors[0]);
     }
 })
