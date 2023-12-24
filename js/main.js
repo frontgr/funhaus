9c076234d1acfe43e7ae7a6e7734501e048d733d
Aleksey02 <lyosha2002stich@gmail.com>
2023-12-24 23:17:20 +0500

    feat: Add swiper instead of slider on main section

--- js/main.js
+++ js/main.js
@@ -20,36 +20,29 @@ btnColorTheme.addEventListener("click", function () {
   }
 });
 
-/* slider */
+
+/*swiper*/
 
 let imageBox = document.querySelector(".main__image__container");
 let imgs = Array.from(imageBox.children); //imageBox.children;
-let currentSlide = 0;
-let maxWidth = imgs.map((img) => img.width).sort(function(a, b){return a-b})[imgs.length-1];
+let minHeight = imgs.map((img) => img.height).sort(function(a, b){return a-b})[0];
+let originalMinHeight = minHeight;
 imgs.forEach((img) => {
-  if (img.width < maxWidth) {
-    img.style.padding = `0 ${(maxWidth - img.width) / 2}px`;
-  }
+    if (img.height > minHeight) {
+      img.style.height = minHeight + "px";
+    }
+  })
+
+const swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  speed: 1600,
+  height: minHeight,
 });
-let interval = setInterval(nextSlide, 3000);
 
 window.addEventListener("resize", () => {
-  maxWidth = imgs.map((img) => img.width).sort()[0];
-  imgs.forEach((img) => {
-    if (img.width < maxWidth) {
-      img.style.padding = `0 ${(maxWidth - img.width) / 2}px`;
-    }
-  });
-  clearInterval(interval);
-  nextSlide();
-  interval = setInterval(nextSlide, 3000);
+  window.location.reload();
 });
-
-function nextSlide() {
-  if (currentSlide < imgs.length - 1) {
-    currentSlide++;
-  } else {
-    currentSlide = 0;
-  }
-  imageBox.style.transform = `translateX(${-maxWidth * currentSlide}px)`;
-}
+/*swiper*/
